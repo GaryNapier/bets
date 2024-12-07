c109c27a6122efba254c41ba70c25cab66851794
Gary Napier <garynapier@garys-mbp-2.home>
2024-12-07 19:51:40 +0000

    update

--- dependencies.R
+++ dependencies.R
@@ -1,2 +1,7 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
+
+library(dplyr)
+library(plotly)
 library(rhino)
+library(readxl)
+library(shinydashboard)
