f7d89e94a0d4242613c53ae58d89107e0f616ea9
Gary Napier <garynapier@garys-mbp-2.home>
2024-12-07 19:53:10 +0000

    initial

---
+++ app/view/clean_validate.R
@@ -0,0 +1,22 @@
+
+# CLEAN AND VALIDATE DATA
+
+
+
+chartUI <- function(id) {
+  # Create unique variable name
+  ns <- NS(id)
+  
+}
+
+
+chartServer <- function(id, x, y, title) {
+  moduleServer(
+    id = id,
+    module = function(input, output, session) {
+      
+      
+      
+    }
+  )
+}
